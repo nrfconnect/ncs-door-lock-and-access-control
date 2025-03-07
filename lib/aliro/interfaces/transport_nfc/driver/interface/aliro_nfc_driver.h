3c980300c1c5458527f29d6b696e399e89b54e29
Marcin Kajor <marcin.kajor@nordicsemi.no>
2025-03-07 13:23:27 +0100

    manifest: Pull Aliro stack NFCTransportManager API changes
    - pull recent changes from Aliro stack that removes
      RecoverPolling() and mOnTimeout callback from NFCTransportManager
      public API
    - adapt door-lock app code and RFAL-based Aliro NFC implementation
    - update Aliro stack binary lib
    
    Signed-off-by: Marcin Kajor <marcin.kajor@nordicsemi.no>

--- lib/aliro/interfaces/transport_nfc/driver/interface/aliro_nfc_driver.h
+++ lib/aliro/interfaces/transport_nfc/driver/interface/aliro_nfc_driver.h
@@ -25,17 +25,6 @@ class NfcDriverImpl;
 class NfcDriver {
 public:
 	/**
-	 * @enum DetectType
-	 * @brief Types of detection commands that can be sent to an NFC tag.
-	 */
-	enum DetectType : uint8_t {
-		/** A command that requires the tag to be moved out of and back into the field to be re-detected. */
-		REQA,
-		/** A command that can wake the tag from a HALT state without a need to move it out of the field. */
-		WUPA
-	};
-
-	/**
 	 * @struct Callbacks
 	 * @brief Struct containing callback functions for NFC  reader driver events.
 	 *
@@ -44,18 +33,12 @@ public:
 	struct Callbacks {
 		/** Called when data is received (RX). */
 		void (*mOnDataReceived)(NfcTransport::Data, int transferError){ nullptr };
-		/** Called when anticollision sequence is completed. */
-		void (*mOnAnticollisionCompleted)(){ nullptr };
-		/** Called when a tag is detected. */
+		/** Called when Tag4 is detected, anticollision is completed and ISO-DEP protocol can be activated. */
 		void (*mOnTagDetected)(){ nullptr };
-		/** Called when the NFC field is turned on. */
-		void (*mOnFieldOn)(){ nullptr };
 		/** Called when data is successfully sent. */
 		void (*mOnSendSuccess)(){ nullptr };
 		/** Called on each error occurrence. */
 		void (*mOnError)(AliroError){ nullptr };
-		/** Called when the tag goes into sleep mode. */
-		void (*mOnTagSleep)(){ nullptr };
 		/** Called on timeout, indicating if the system is in sleep. */
 		void (*mOnTimeout)(bool inSleep){ nullptr };
 	};
@@ -76,38 +59,16 @@ public:
 	AliroError Send(NfcTransport::Data data, uint32_t maximumFrameDelayTime);
 
 	/**
-	 * @brief Turns the reader's NFC field on.
+	 * @brief Enables the NFC.
 	 * @return AliroError status of the operation.
 	 */
-	AliroError FieldOn();
+	AliroError NfcOn();
 
 	/**
-	 * @brief Turns the the reader's NFC field off.
+	 * @brief Disables the NFC.
 	 * @return AliroError status of the operation.
 	 */
-	AliroError FieldOff();
-
-	/**
-	 * @brief Starts the detection of the NFC tag using the specified detection type.
-	 * @param type The type of detection command to use.
-	 * @return AliroError status of the operation.
-	 */
-	AliroError TagDetect(DetectType type);
-
-	/**
-	 * @brief Starts the anticollision sequence.
-	 * @return AliroError status of the operation.
-	 */
-	AliroError StartAnticollision();
-
-	/**
-	 * @brief Recovers the NFC driver polling after a specified delay.
-	 * @param delayMs Delay in milliseconds before recovering.
-	 * @param type Type of detection.
-	 * @param tagSleep A flag indicating if the sleep command must be sent to the tag.
-	 * @return AliroError Status of the restart.
-	 */
-	AliroError RecoverPolling(uint32_t delayMs, DetectType type, bool tagSleep);
+	AliroError NfcOff();
 
 protected:
 	NfcDriver() = default;
