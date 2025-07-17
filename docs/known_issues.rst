3d2b6808e55db07db5201e6fcf7b8ba78b60fffa
NordicBuilder <nordicbuilder@nordicsemi.com>
2025-07-17 17:28:51 +0200

    CI/CD: Promote lib files to ncs-door-lock-app @881628583d2b
    Source commit ID: @881628583d2b
    
    Signed-off-by: NordicBuilder <nordicbuilder@nordicsemi.com>

--- docs/known_issues.rst
+++ docs/known_issues.rst
@@ -21,45 +21,22 @@ A known issue can list one or both of the following entries:
   Some known issues have a workaround.
   Sometimes, they are discovered later and added over time.
 
-The |APP_NAME|
-**************
-
-`v0.2.0`
-
-AL-239: Occasional timeout in the reader when executing the RD-NFC-STDTXN-2.0 test from the Test Harness
-  When executing the RD-NFC-STDTXN-2.0 test case, the reader might report an RX timeout error code in the serial console.
-  For more details, see `Test Harness issue #191`_.
-
-`v0.2.0`
-
-AL-282: Undefined access decision when executing the RD-NFC-STDTXN-2.0 Test Harness case in a loop
-  When executing the RD-NFC-STDTXN-2.0 test case multiple times in a row (with valid credentials provisioned), the reader might not report the access decision at all.
-  As a result, neither ``ACCESS GRANTED`` nor ``ACCESS DENIED`` log is displayed in the serial output of the reader under test.
-
-`v0.1.0` `v0.2.0`
+The |APP_NAME| v0.1.0
+*********************
 
 AL-148: The RD-NFC-STDTXN-1.0 test case fails when the NFC module ST X-NUCLEO-NFC05A1 is in use
   Testing RD-NFC-STDTXN-1.0 with the NFC module NFC05A1 results in failure.
   The issue arises from an error indicated by the test harness, which detects the presence of an invalid TLV tag in the payload received from the Device Under Test (DUT).
 
   **Workaround:** Switch to X-NUCLEO-NFC09A1, a newer, recommended revision of the NFC ST module.
-  Attach the X-NUCLEO-NFC09A1 shield to the supported Nordic development kit, rebuild the firmware with the ``CONFIG_ST25R200_DRV`` Kconfig option enabled, and reflash the DK.
-
-`v0.1.0` `v0.2.0`
+  Attach X-NUCLEO-NFC09A1 shield to the nRF54L15 DK, rebuild the firmware with the ``CONFIG_ST25R200_DRV`` Kconfig option enabled, and re-flash the nRF54L15 DK.
 
 AL-158: Access control is insufficient due to relying only on signature verification
   The reader device does not have the capability to configure additional access rules.
   Access Manager is not implemented.
 
-`v0.1.0` `v0.2.0`
-
 AL-159: Cannot provision multiple user devices
   The system allows provisioning of only one user device to the reader device.
 
-`v0.1.0` `v0.2.0`
-
 AL-161: The RD-NFC-STDTXN-2.0 [X-NUCLEO-NFC08A1] test exhibits a delay in the transaction initiation step
   During the RD-NFC-STDTXN-2.0 test execution, there is a noticeable delay of a few seconds after the transaction initiation step.
-
-  **Workaround:** Switch to X-NUCLEO-NFC09A1, a newer, recommended revision of the NFC ST module.
-  Attach the X-NUCLEO-NFC09A1 shield to the supported Nordic development kit, rebuild the firmware with the ``CONFIG_ST25R200_DRV`` Kconfig option enabled, and reflash the DK.
---
+++ interfaces/transport/nfc/isodep/interface/aliro_isodep.h
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+#pragma once
+
+#include "isodep_config.h"
+
+#include "aliro/errors.h"
+#include "aliro/types.h"
+
+namespace Aliro {
+
+class IsoDepImpl;
+
+/**
+ * @class IsoDep
+ * @brief Interface class for handling NFC ISO-DEP layer.
+ *
+ * IsoDep provides an interface for NFC data exchange based on the ISO-DEP protocol.
+ * It allows for initialization, data preparation, and handling of ISO-DEP communication events.
+ */
+class IsoDep {
+public:
+	/**
+	 * @enum SelectStatus
+	 * @brief Status codes for tag selection.
+	 */
+	enum SelectStatus : uint8_t {
+		/** Selection successful. */
+		StatusOk,
+		/** Bitrate divisor unsupported. */
+		UnsupportedBitrateDivisor
+	};
+
+	/**
+	 * @struct Callbacks
+	 * @brief Struct containing callback functions for NFC events.
+	 *
+	 * This struct holds pointers to functions that are called on specific NFC events.
+	 */
+	struct Callbacks {
+		/** Called when TX data is ready to be sent over the transport medium. */
+		void (*mOnTxDataReady)(Data, uint32_t maxFrameDelayTime){ nullptr };
+		/** Called when new processed data (RX) is available for reading. */
+		void (*mOnRxDataAvailable)(Data){ nullptr };
+		/** Called when a tag is selected. */
+		void (*mOnTagSelected)(SelectStatus){ nullptr };
+		/** Called on each error occurrence. */
+		void (*mOnError)(AliroError){ nullptr };
+	};
+
+	/**
+	 * @brief Initializes the IsoDep component with specified callbacks.
+	 * @param callbacks Struct containing callback functions.
+	 * @return AliroError status of the initialization.
+	 */
+	AliroError Init(Callbacks callbacks);
+
+	/**
+	 * @brief Prepares data for transmission. Once data is ready to be sent, the mOnTxDataReady callback must be
+	 * called, so that the underlying driver can send the data through the transport medium.
+	 * @param data Data to be prepared.
+	 * @return AliroError status of the operation.
+	 */
+	AliroError PrepareData(Data data);
+
+	/**
+	 * @brief Prepares the RATS (Request for Answer To Select) command payload. Once data is ready to be sent,
+	 * the mOnTxDataReady callback must be called, so that the underlying driver can RATS command through the
+	 * transport medium.
+	 * @return AliroError status of the operation.
+	 */
+	AliroError PrepareRats();
+
+	/**
+	 * @brief Handles the incoming data in the ISO-DEP layer. This function is supposed to be called with RX data
+	 * forwarded from the transport medium.
+	 * @param data Received data.
+	 * @param transferError Error status of the data transfer.
+	 * @return AliroError status of the operation.
+	 */
+	AliroError HandleReceivedData(Data data, int transferError);
+
+	/**
+	 * @brief Reports a timeout event. This function is supposed to be called to notify the ISO-DEP layer about the
+	 * timeout that happened in the transport medium layer.
+	 * @return AliroError status of the operation.
+	 */
+	AliroError ReportTimeout();
+
+protected:
+	IsoDep() = default;
+	IsoDep(const IsoDep &) = delete;
+	IsoDep(IsoDep &&) = delete;
+	~IsoDep() = default;
+	IsoDep &operator=(const IsoDep &) = delete;
+	IsoDep &operator=(IsoDep &&) = delete;
+
+	Callbacks mCallbacks{};
+
+private:
+	IsoDepImpl *Impl();
+};
+
+/**
+ * @brief Returns the unique IsoDep implementation object that can be used by the client code.
+ * @return Unique IsoDep object reference.
+ */
+extern IsoDep &IsoDepInstance();
+
+} // namespace Aliro
