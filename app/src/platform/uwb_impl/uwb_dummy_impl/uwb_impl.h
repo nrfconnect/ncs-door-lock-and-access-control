06d617d3ffc4079dc03fc7a0ca68e2bcd390a60c
Miecznik, Rafał <rafal.miecznik@nordicsemi.com>
2025-07-18 14:45:17 +0200

    CI/CD: Promote lib files to ncs-door-lock-app @
    Source commit ID: @
    
    Signed-off-by: Miecznik, Rafał <rafal.miecznik@nordicsemi.com>

---
+++ app/src/platform/uwb_impl/uwb_dummy_impl/uwb_impl.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#pragma once
+
+#include "aliro/errors.h"
+#include "uwb/uwb.h"
+
+#include <cstddef>
+
+namespace Aliro::Uwb {
+
+/**
+ * @class UltraWideBandImpl
+ * @brief Dummy implementation of the UltraWideBand interface.
+ *
+ * This class provides the dummy implementation of the UltraWideBand interface, handling UWB operations
+ * such as initialization, ranging session management, and BLE message handling.
+ */
+class UltraWideBandImpl : public UltraWideBand<UltraWideBandImpl> {
+public:
+	/**
+	 * @brief Gets the instance of the UltraWideBand implementation.
+	 *
+	 * @return The instance of the UltraWideBand implementation.
+	 */
+	static UltraWideBandImpl &Instance()
+	{
+		static UltraWideBandImpl sInstance;
+		return sInstance;
+	}
+
+	AliroError _Init([[maybe_unused]] const Callbacks &) const { return ALIRO_ERROR_NOT_IMPLEMENTED; }
+	AliroError _Deinit() const { return ALIRO_ERROR_NOT_IMPLEMENTED; }
+	void
+	_BleTimeSync() const { /* No operation for dummy implementation; override in derived classes if needed. */ };
+
+	AliroError _HandleBleMessage([[maybe_unused]] const uint8_t *, [[maybe_unused]] size_t) const
+	{
+		return ALIRO_ERROR_NOT_IMPLEMENTED;
+	}
+
+	AliroError _ConfigureRangingSession([[maybe_unused]] SessionIdentifier,
+					    [[maybe_unused]] const CryptoTypes::Ursk &,
+					    [[maybe_unused]] const void *) const
+	{
+		return ALIRO_ERROR_NOT_IMPLEMENTED;
+	}
+
+	AliroError _InitiateRangingSession() const { return ALIRO_ERROR_NOT_IMPLEMENTED; }
+
+	AliroError _TerminateRangingSession() const { return ALIRO_ERROR_NOT_IMPLEMENTED; }
+	AliroError _SuspendRangingSession() const { return ALIRO_ERROR_NOT_IMPLEMENTED; }
+	AliroError _ResumeRangingSession() const { return ALIRO_ERROR_NOT_IMPLEMENTED; }
+
+	// Delete copy and move constructors and assignment operators.
+	UltraWideBandImpl(const UltraWideBandImpl &) = delete;
+	UltraWideBandImpl &operator=(const UltraWideBandImpl &) = delete;
+	UltraWideBandImpl(UltraWideBandImpl &&) = delete;
+	UltraWideBandImpl &operator=(UltraWideBandImpl &&) = delete;
+
+private:
+	UltraWideBandImpl() = default;
+	~UltraWideBandImpl() = default;
+};
+
+} // namespace Aliro::Uwb
