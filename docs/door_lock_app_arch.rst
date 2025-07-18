22aac2143be937490258a0148f7b885c6be94764
Miecznik, Rafał <rafal.miecznik@nordicsemi.com>
2025-07-18 18:38:23 +0200

    CI/CD: Promote lib files to ncs-door-lock-app @45786c0ecc18
    Source commit ID: @45786c0ecc18
    
    Signed-off-by: Miecznik, Rafał <rafal.miecznik@nordicsemi.com>

--- docs/door_lock_app_arch.rst
+++ docs/door_lock_app_arch.rst
@@ -20,7 +20,7 @@ The Aliro stack implements the Access Protocol logic, Aliro-specific cryptograph
 The interfaces layer is a bridge connecting the Aliro stack to the Zephyr OS modules through specific backends that implement the following components required by the Aliro: crypto, NFC and, ultra wideband (UWB).
 This layer additionally allows to utilize other, custom backends for the crypto, NFC and UWB components by implementing the provided API.
 By default, the |APP_NAME| uses backends shown in the :ref:`architecture overview <arch_overview>`.
-Both the Aliro stack and the interfaces layers are placed in the :file:`lib/aliro` directory.
+The Aliro stack library files are placed in the :file:`lib/aliro` directory.
 
 The RF Abstraction Layer (NFC RFAL) handles communication with the STMicroelectronics :ref:`NFC integrated circuit (IC) <hw_requirements_nfc_reader>`.
 This layer contains drivers, platform abstraction layer (PAL) and the NFC protocol stack, covering evrything from physical characteristic to the application layer.
@@ -30,5 +30,48 @@ Communication with external IC's is done through the Serial Peripheral Interface
 The `Platform Security Architecture (PSA)`_ API provides a portable programming interface for cryptographic operations and key storage across a wide range of hardware.
 It is designed to be user-friendly while still providing access to the low-level primitives essential for modern cryptography.
 
+The `Bluetooth LE Controller`_ provides the necessary functionality for Bluetooth LE communication.
+Bluetooth LE transport is supported on the `nRF5340 DK`_ platform and is configured to operate in the peripheral role, allowing the device to advertise its presence and accept connections from Bluetooth LE central devices.
+The advertising payload is generated according to the Aliro specification, based on the ``reader group identifier`` and ``reader group sub-identifier``.
+The payload updates automatically whenever these values change, ensuring that the device always advertises the latest group information.
+
 .. note::
-   In the current implementation, the Bluetooth® LE transport protocol and UWB proximity sensing are not supported.
+   The device is preconfigured with default values for the ``reader group identifier`` and ``reader group sub-identifier``.
+   You can provide custom values using the ``dl install`` command through the console to override the defaults.
+   For more information about setting these values, see the :ref:`testing_environment_configuration` section.
+
+When a Bluetooth LE central device connects, the Aliro stack manages the Bluetooth LE session, handling connection events, security, and data exchange over a dedicated L2CAP channel with Aliro-specific GATT services.
+Each session uses unique keys, which are destroyed after termination.
+In case of NFC transport, only one session can be active at a time.
+For Bluetooth LE transport, the maximum number of concurrent sessions is limited by the ``ALIRO_BLE_TP_MAX_SESSIONS`` Kconfig option and defaults to the value of ``BT_MAX_CONN``.
+
+To configure number of Bluetooth LE sessions, set the ``BT_MAX_CONN`` to maximum number of connections and optionally use the ``ALIRO_BLE_TP_MAX_SESSIONS`` to limit the number of sessions.
+
+.. note::
+   Each session consumes resources, such as RAM and PSA key slots, therefore the maximum number of the active sessions should be determined empirically based on the requirements of the final application.
+
+You can configure Bluetooth LE transport parameters, such as buffer sizes, MTU, GATT database, L2CAP channels, TX power, and PHY through Kconfig (see :file:`lib/aliro/Kconfig.ble.defconfig`).
+
+.. note::
+   In the current implementation, the UWB proximity sensing is not supported.
+
+.. _Access_decision_indicator:
+
+Access decision indicator
+=========================
+
+When access is granted, a generic indicator is activated for a predefined period to visually confirm successful authentication.
+By default, this indicator is a dedicated LED, but it can be adapted to other types of indicators such as a buzzer.
+
+You can set the duration of the indication in the :file:`app/src/platform/access_decision_indicator/Kconfig` file.
+Use the ``RESET_ACCESS_DECISION_INDICATOR_STATE_DELAY_MS`` Kconfig option to specify the time in milliseconds.
+
+You can select the hardware resource used for this indication by going to the device tree source file and setting the ``access_decision_indicator`` property in the corresponding node.
+
+.. code-block:: dts
+
+   access_decision_indicator: access_decision_indicator {
+       status = "okay";
+       compatible = "access-decision-indicator";
+       gpios = <&gpio2 10 GPIO_ACTIVE_HIGH>;
+   };
---
+++ lib/aliro/interfaces/uwb/uwb.h
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+#pragma once
+
+#include "aliro/errors.h"
+#include "aliro/types.h"
+
+#include <cstddef>
+
+namespace Aliro::Uwb {
+
+/**
+ * @class UltraWideBand
+ * @brief Interface class for managing ultra wideband (UWB) module and ranging session.
+ *
+ * This class provides methods to initialize, deinitialize, and manage UWB ranging sessions,
+ * as well as handling BLE messages and time synchronization.
+ */
+template <typename IfaceImpl> class UltraWideBand {
+public:
+	using SessionIdentifier = uint32_t;
+
+	/**
+	 * @struct Callbacks
+	 * @brief Struct containing callback functions for UWB events.
+	 *
+	 * This struct holds pointers to functions that are called on specific UWB events,
+	 * such as transmitting BLE messages and receiving ranging data.
+	 */
+	struct Callbacks {
+		/**
+		 * @brief Callback to transmit a BLE message.
+		 *
+		 * This callback is invoked when the UWB module needs to send a BLE message
+		 * as part of the Aliro protocol flow. The message must conform to the format
+		 * specified in Table 11-10 of the Aliro specification, and the payload must stay
+		 * unencrypted.
+		 *
+		 * @param userData User-defined context data.
+		 * @param data Pointer to the formatted BLE message data.
+		 * @param length Size of the message data in bytes.
+		 */
+		void (*mTransmitBleMessage)(void *userData, const uint8_t *data, size_t length){ nullptr };
+		/**
+		 * @brief Callback to receive ranging data.
+		 *
+		 * This callback is invoked when the UWB module receives ranging data.
+		 *
+		 * @param data Pointer to the received ranging data.
+		 * @param length Size of the received data in bytes.
+		 */
+		void (*mRangingData)(const uint8_t *data, size_t length){ nullptr };
+	};
+
+	/**
+	 * @brief Initializes the UltraWideBand module.
+	 *
+	 * This method can be used to initialize the UWB module, setting up necessary configurations
+	 * and preparing it for use. It should be called before any other UWB operations
+	 * are performed.
+	 *
+	 * @param callbacks Struct containing callback functions for UWB events.
+	 *
+	 * @return ALIRO_NO_ERROR on success, or an error code on failure.
+	 */
+	AliroError Init(const Callbacks &callbacks) { return Impl()->_Init(callbacks); }
+
+	/**
+	 * @brief Deinitializes the UltraWideBand module.
+	 *
+	 * This method deinitializes the UWB hardware and releases any resources it was using.
+	 *
+	 * @return ALIRO_NO_ERROR on success, or an error code on failure.
+	 */
+	AliroError Deinit() { return Impl()->_Deinit(); };
+
+	/**
+	 * @brief Triggers Bluetooth LE (BLE) and UWB time synchronization.
+	 *
+	 * This method is called just after BLE connection establishment (CONNECT_IND event). In the method
+	 * implementaion the Procedure 0: Bluetooth LE (BLE) Timesync can be started according to the chapter 19.4.4.1
+	 * in Digital Key Release 3.0 Technical Specification Version 1.1.0.
+	 *
+	 * @note In implementation of this method for example, the GPIO pin can be used to trigger time
+	 * synchronnization between BLE and UWB modules.
+	 */
+	void BleTimeSync() const { Impl()->_BleTimeSync(); }
+
+	/**
+	 * @brief Transmits deserialized and decrypted Aliro BLE messages to the UWB module.
+	 *
+	 * This method sends the UWB Ranging Service and Notifications (with ID == Ranging) to the UWB module.
+	 * The message conform to the format specified in Table 11-10 of the Aliro specification, and the payload is
+	 * decrypted.
+	 *
+	 * @param data Pointer to data to be transmitted.
+	 * @param length Length of the data in bytes.
+	 *
+	 * @return ALIRO_NO_ERROR if the transmission was successful, an error code otherwise.
+	 */
+	AliroError HandleBleMessage(const uint8_t *data, size_t length)
+	{
+		return Impl()->_HandleBleMessage(data, length);
+	}
+
+	/**
+	 * @brief Configures the ranging session with the provided URSK in plaintext.
+	 *
+	 * This method sets up the ranging session using the provided URSK (UWB Ranging Secret Key).
+	 *
+	 * @param sessionId The session identifier for the ranging session (Chapter 11.7.2.1.3 in the Aliro spec.).
+	 * @param ursk Reference to the URSK.
+	 * @param sessionUserData Pointer to user data.
+	 *
+	 * @return ALIRO_NO_ERROR on success, or an error code on failure.
+	 */
+	AliroError ConfigureRangingSession(SessionIdentifier sessionId, const CryptoTypes::Ursk &ursk,
+					   void *sessionUserData)
+	{
+		return Impl()->_ConfigureRangingSession(sessionId, ursk, sessionUserData);
+	}
+
+	/**
+	 * @brief Initiates a UWB ranging session.
+	 *
+	 * This method starts a UWB ranging session by creating the M1 message and sending it
+	 * over the BLE interface to the User Device.
+	 *
+	 * @return ALIRO_NO_ERROR if the session was initiated successfully, an error code otherwise.
+	 */
+	AliroError InitiateRangingSession() { return Impl()->_InitiateRangingSession(); }
+
+	/**
+	 * @brief Terminates the current UWB ranging session.
+	 *
+	 * This method terminates the current UWB ranging session and releases any resources it was using.
+	 *
+	 * @return ALIRO_NO_ERROR if the session was terminated successfully, an error code otherwise.
+	 */
+	AliroError TerminateRangingSession() { return Impl()->_TerminateRangingSession(); }
+
+	/**
+	 * @brief Suspends the current UWB ranging session.
+	 *
+	 * This method suspends the current UWB ranging session. It can be used to pause the session temporarily, to
+	 * resume the session use `AliroUwbResumeRangingSession()`.
+	 *
+	 * @param force If true, forces the suspension even if there are ongoing operations.
+	 *
+	 * @return ALIRO_NO_ERROR if the session was suspended successfully, an error code otherwise.
+	 */
+	AliroError SuspendRangingSession(bool force = false) { return Impl()->_SuspendRangingSession(force); }
+
+	/**
+	 * @brief Resumes the suspended UWB ranging session.
+	 *
+	 * This method resumes the UWB ranging session that was previously suspended by
+	 * `AliroUwbSuspendRangingSession()`.
+	 *
+	 * @return ALIRO_NO_ERROR if the session was resumed successfully, an error code otherwise.
+	 */
+	AliroError ResumeRangingSession() { return Impl()->_ResumeRangingSession(); }
+
+protected:
+	IfaceImpl *Impl() { return static_cast<IfaceImpl *>(this); }
+	const IfaceImpl *Impl() const { return static_cast<const IfaceImpl *>(this); }
+};
+
+} // namespace Aliro::Uwb
