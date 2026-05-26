146724a62908110ebd83664ff665c533f6a2f29c
Bartosz Gentkowski <bartosz.gentkowski@nordicsemi.no>
2026-05-26 02:40:48 +0200

    doc: prepare docs for new portal
    Doc changes required for the new Sphinx theme.
    Also, updates to the workflows.
    
    Signed-off-by: Bartosz Gentkowski <bartosz.gentkowski@nordicsemi.no>

--- docs/conf.py
+++ docs/conf.py
@@ -21,10 +21,8 @@ from pathlib import Path
 project = 'nRF Door Lock and Access Control Add-on'
 copyright = '2026, Nordic Semiconductor'
 author = 'Nordic Semiconductor'
-
-# The full version, including alpha/beta/rc tags
-release = ' '
-
+release = 'Latest'
+version = 'Latest'
 
 # -- General configuration ---------------------------------------------------
 
