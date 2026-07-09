cc2ee993f71d6a6a5fe1214e272b1e675c1def60
Adrian Gielniewski <adrian.gielniewski@nordicsemi.no>
2026-07-09 14:31:33 +0200

    interface_impl: Add common Reader interface implementation
    Extract common `Aliro::Interface::Reader` implementation from applications.
    
    Signed-off-by: Adrian Gielniewski <adrian.gielniewski@nordicsemi.no>

--- subsys/aliro/interface_impl/reader/reader.cpp
+++ subsys/aliro/interface_impl/reader/reader.cpp
@@ -4,13 +4,9 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include "aliro/interface.h"
+#include <aliro/interface.h>
 
 #include <reader_storage/reader.h>
-
-#include <zephyr/logging/log.h>
-
-LOG_MODULE_REGISTER(interface_reader, CONFIG_DOOR_LOCK_APP_LOG_LEVEL);
 
 namespace Aliro::Interface::Reader {
 
@@ -26,31 +22,31 @@ AliroError GetPublicKey(CryptoTypes::PublicKey &publicKey)
 
 bool IsCertificateProvisioned()
 {
-#ifdef CONFIG_DOOR_LOCK_READER_CERTIFICATE
+#ifdef CONFIG_DOOR_LOCK_ALIRO_READER_STORAGE_CERTIFICATE_SUPPORT
 	return DoorLock::ReaderStorage::IsCertificateSet() && DoorLock::ReaderStorage::IsIssuerPublicKeySet();
-#else // CONFIG_DOOR_LOCK_READER_CERTIFICATE
+#else // CONFIG_DOOR_LOCK_ALIRO_READER_STORAGE_CERTIFICATE_SUPPORT
 	return false;
-#endif // CONFIG_DOOR_LOCK_READER_CERTIFICATE
+#endif // CONFIG_DOOR_LOCK_ALIRO_READER_STORAGE_CERTIFICATE_SUPPORT
 }
 
 AliroError GetIssuerPublicKey(CryptoTypes::PublicKey &publicKey)
 {
-#ifdef CONFIG_DOOR_LOCK_READER_CERTIFICATE
+#ifdef CONFIG_DOOR_LOCK_ALIRO_READER_STORAGE_CERTIFICATE_SUPPORT
 	return DoorLock::ReaderStorage::GetIssuerPublicKey(publicKey);
-#else // CONFIG_DOOR_LOCK_READER_CERTIFICATE
+#else // CONFIG_DOOR_LOCK_ALIRO_READER_STORAGE_CERTIFICATE_SUPPORT
 	ARG_UNUSED(publicKey);
 	return ALIRO_ERROR_NOT_IMPLEMENTED;
-#endif // CONFIG_DOOR_LOCK_READER_CERTIFICATE
+#endif // CONFIG_DOOR_LOCK_ALIRO_READER_STORAGE_CERTIFICATE_SUPPORT
 }
 
 AliroError GetCertificate(ConstData &certificate)
 {
-#ifdef CONFIG_DOOR_LOCK_READER_CERTIFICATE
+#ifdef CONFIG_DOOR_LOCK_ALIRO_READER_STORAGE_CERTIFICATE_SUPPORT
 	return DoorLock::ReaderStorage::GetCertificate(certificate);
-#else // CONFIG_DOOR_LOCK_READER_CERTIFICATE
+#else // CONFIG_DOOR_LOCK_ALIRO_READER_STORAGE_CERTIFICATE_SUPPORT
 	ARG_UNUSED(certificate);
 	return ALIRO_ERROR_NOT_IMPLEMENTED;
-#endif // CONFIG_DOOR_LOCK_READER_CERTIFICATE
+#endif // CONFIG_DOOR_LOCK_ALIRO_READER_STORAGE_CERTIFICATE_SUPPORT
 }
 
 } // namespace Aliro::Interface::Reader
